51c9a8b9afe4d3bf38adc0216e848af5c5ee9a13
Eklak Dangaura <azens1995@gmail.com>
2023-09-17 10:55:42 +0545

    feat: first method to configure inmemordy client details service

--- src/main/java/dev/eklak/springoautauthserver/config/AuthServerConfig.java
+++ src/main/java/dev/eklak/springoautauthserver/config/AuthServerConfig.java
@@ -1,11 +1,18 @@
 package dev.eklak.springoautauthserver.config;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
 
 // EnableAuthorizationServer instructs Spring Boot to enable configuration specific
 // to the OAuth 2 authorization server
@@ -21,5 +28,29 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     public void configure(
         AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
+    }
+
+    // Setting up ClientDetailsService instance
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws
+        Exception {
+        /*
+        // Method 1
+        // Creates an instance using the ClientDetailsService implementation
+        var service = new InMemoryClientDetailsService();
+
+        // Creates an instance of ClientDetails and set the required information
+        var cd = new BaseClientDetails();
+        cd.setClientId("client");
+        cd.setClientSecret("secret");
+        cd.setScope(List.of("read"));
+        cd.setAuthorizedGrantTypes(List.of("password"));
+
+        // Adds the client details to InMemoryClientDetailsService
+        service.setClientDetailsStore(Map.of("client", cd));
+
+        // Configure client details service for use by our authorization server
+        clients.withClientDetails(service);
+        */
     }
 }
